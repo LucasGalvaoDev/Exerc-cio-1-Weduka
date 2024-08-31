a9a33642230a9369780623fbdb0d130bdf78ec62
LucasGalvaoDev <121260233+LucasGalvaoDev@users.noreply.github.com>
2024-08-31 16:27:12 -0300

    projeto finalizado

---
+++ WedukaEx1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using WedukaEx1.Validators;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            var validador = new ValidadorDeColchetes();
+
+            Console.Write("Insira uma sequência de colchetes, chaves ou parênteses: ");
+            Console.WriteLine();
+            Console.Write("Obs.: O código só será finalizado ao digitar sair.");
+            Console.WriteLine();
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "sair")
+            {
+                break;
+            }
+
+            bool isValid = validador.IsValidBrackets(input);
+
+            Console.WriteLine($"A sequência '{input}' é {(isValid ? "valida" : "não válida")}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ WedukaEx1/Validators/ValidadorDeColchetes.cs
@@ -0,0 +1,61 @@
+﻿namespace WedukaEx1.Validators
+{
+    class ValidadorDeColchetes
+    {
+        public bool IsValidBrackets(string input)
+        {
+            // Verificar se os caracteres de entrada são válidos
+            foreach (char c in input)
+            {
+                if (!IsValidCharacter(c))
+                {
+                    return false;
+                }
+            }
+
+            // Verificar se todos os que foram abertos estão sendo fechados
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in input)
+            {
+                if (IsOpenBracket(c))
+                {
+                    stack.Push(c);
+                }
+                else if (IsCloseBracket(c))
+                {
+                    if (stack.Count == 0 || !Matches(stack.Pop(), c))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+
+        #region Validadores de caracteres
+
+        private bool IsValidCharacter(char c)
+        {
+            return c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}';
+        }
+
+        private bool IsOpenBracket(char c)
+        {
+            return c == '(' || c == '[' || c == '{';
+        }
+
+        private bool IsCloseBracket(char c)
+        {
+            return c == ')' || c == ']' || c == '}';
+        }
+
+        private bool Matches(char open, char close)
+        {
+            return (open == '(' && close == ')') ||
+                   (open == '[' && close == ']') ||
+                   (open == '{' && close == '}');
+        }
+        #endregion
+    }
+}
